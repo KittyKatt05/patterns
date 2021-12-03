76250922d32f97e241b3e683d768a26a6b9bfdb9
Catarina <1201338@isep.ipp.pt>
2021-12-03 15:05:42 +0000

    Created StringDrink class

---
+++ src/main/java/StringDrink.java
@@ -0,0 +1,17 @@
+public class StringDrink {
+
+    String drink;
+
+    public StringDrink(String str){
+        drink = str;
+    }
+
+    public String getText(){
+        return drink;
+    }
+
+    public void setText(String drink){
+        this.drink = drink;
+    }
+
+}
---
+++ src/main/java/StringTranformer.java
@@ -0,0 +1,4 @@
+public interface StringTranformer {
+
+    void execute(StringDrink drink);
+}
